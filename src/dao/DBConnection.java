0598935eff7274f1f25e3713b3c1680d42724b9c
Mar Simó <astraproductionscontact@gmail.com>
2026-01-03 12:53:50 +0100

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL="jdbc:mariadb://192.168.68.53:3306/myapp";
+    private static final String URL="jdbc:mariadb://localhost:3306/myapp";
     private static final String USER="backendMyApp";
-    private static final String PASSWORD="58Jwy;;>22(9";
+    private static final String PASSWORD="TU_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
